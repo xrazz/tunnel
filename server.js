760ce9a812d1e4a71789712f28267c904d393885
xrazz <rajtripathi2580@gmail.com>
2025-10-22 12:28:02 +0530

    fixed port issue in render

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const { Server } = require('socket.io');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
